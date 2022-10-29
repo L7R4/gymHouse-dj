08c49b4341f8b0d8f7148211d6e0879451bfd228
Lautaro <lautaro.rodriguez553@gmail.com>
2022-10-29 11:24:46 -0300

    Creacion de modal para inciar el login/logout

--- gymHouse/static/js/modal_login.js
+++ gymHouse/static/js/modal_login.js
@@ -1,5 +1,5 @@
 button_login = document.querySelector(".main__button");
-
+modal_login = document.querySelector(".modal_login")
 button_login.addEventListener("click", () =>{
-    alert("sadsa")
+    modal_login.classList.toggle("active")
 })
