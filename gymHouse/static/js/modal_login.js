8e11271386831cc76a9450e9fa2b194a01bf1fce
Rodriguez Lautaro <lautaro.rodriguez553@gmail.com>
2022-11-09 21:25:44 -0300

    mejora de index

--- gymHouse/static/js/modal_login.js
+++ gymHouse/static/js/modal_login.js
@@ -3,3 +3,9 @@ modal_login = document.querySelector(".modal_login")
 button_login.addEventListener("click", () =>{
     modal_login.classList.toggle("active")
 })
+
+login_cruz = document.querySelector(".login-cruz");
+modal_login = document.querySelector(".modal_login")
+login_cruz.addEventListener("click", () =>{
+    modal_login.classList.toggle("active")
+})
