bcaaf9e0a5fbaad8175fc6593597fe6484b3c72a
Rodriguez Lautaro <lautaro.rodriguez553@gmail.com>
2022-11-10 22:06:57 -0300

    nose 321

--- gymHouse/static/js/alumnosEntrenar.js
+++ gymHouse/static/js/alumnosEntrenar.js
@@ -3,9 +3,30 @@ const ejercicio_pasosid= document.getElementById("ejercicio_pasosid")
 
 ejercicios.forEach(element => {
     element.addEventListener("click", ()=>{
+        console.table(element)
         divcerrado.classList.add("active")
-        let ejer_clone = element.cloneNode(true)
-        ejercicio_pasosid.insertAdjacentElement('beforebegin', ejer_clone)
-        // ejercicio_pasosid
+        if (!element.children[0].checked){
+            let ejer_clone = element.cloneNode(true)
+    
+            let pasos = ejer_clone.children[2]
+            let video = ejer_clone.children[3]
+    
+            console.log(pasos)
+            console.log(video)
+    
+            ejercicio_pasosid.appendChild(pasos)
+            pasos.classList.add("active")
+            
+            ejercicio_videoid.appendChild(video)
+            video.classList.add("active")
+
+            ejercicio_pasosid.insertAdjacentElement('beforebegin', ejer_clone)
+        }
+        
+        
     })
 })
+
+// function desactiveDetalle() {
+//     detalle.forEach(element => element.classList.remove("active"));
+//   }
